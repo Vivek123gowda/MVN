f09ee98666567ca81d9e8fc33a7249cc6ae49266
Vivek <Vivek@LAPTOP-HR8TACRF>
2024-10-23 12:36:58 +0530

    First commit

---
+++ VIV/src/test/java/com/actitime/test/Createcustomer.java
@@ -0,0 +1,14 @@
+package com.actitime.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Createcustomer {
+	@Test
+	
+	public void create() {
+		Reporter.log("createcustomer",true);
+		
+	}
+
+}
